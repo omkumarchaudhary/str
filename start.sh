09abc66d9344c9e9b2c9a33d39f9466a13200dea
Om Kumar Chaudhary <127199200+omkumarchaudhary@users.noreply.github.com>
2025-05-16 18:01:28 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,21 @@
 
 while true
 do
-  echo "Starting low-bitrate stream..."
+  echo "Starting stream..."
 
   ffmpeg \
-    -re -loop 1 -framerate 1 -i background.jpg \
-    -stream_loop -1 -i music.mp3 \
-    -c:v libx264 -preset ultrafast -tune stillimage \
-    -b:v 40k -maxrate 40k -bufsize 80k \
-    -s 426x240 \
-    -c:a aac -b:a 48k \
-    -f flv "rtmp://x.rtmp.youtube.com/live2/6y98-mazq-py6b-kayt-dzz2"
+    -re -loop 1 -framerate 30 -i background.jpg \         # 30 FPS for smooth video
+    -stream_loop -1 -i music.mp3 \                        # loop audio infinitely
+    -c:v libx264 -preset veryfast -tune stillimage \     # encode video fast & optimized for still images
+    -pix_fmt yuv420p \                                    # pixel format compatible with YouTube
+    -c:a aac -b:a 128k -ar 44100 \                        # AAC audio with standard sample rate
+    -f flv \
+    -maxrate 3000k -bufsize 6000k \                       # control bitrate to avoid spikes
+    -g 60 \                                               # GOP size, 2 sec keyframes at 30fps (good for streaming)
+    "rtmp://x.rtmp.youtube.com/live2/6y98-mazq-py6b-kayt-dzz2"
 
-  echo "Stream ended. Restarting in 10 seconds..."
+  echo "Restarting in 10 seconds..."
   sleep 10
 done
+
 
