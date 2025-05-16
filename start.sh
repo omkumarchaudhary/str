4ebb9e90b9caad8c7c2e6ac152aa62668d94b2e6
Om Kumar Chaudhary <127199200+omkumarchaudhary@users.noreply.github.com>
2025-05-16 16:41:17 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ do
     -c:v libx264 -preset veryfast -tune stillimage \
     -c:a aac -b:a 128k \
     -shortest \
-    -f flv "rtmp://x.rtmp.youtube.com/live2"
+    -f flv "rtmp://x.rtmp.youtube.com/live2/6y98-mazq-py6b-kayt-dzz2"
 
   echo "Restarting in 10 seconds..."
   sleep 10
