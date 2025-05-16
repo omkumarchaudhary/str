e5b44acd6356192e8b29b44eb5ff6d29b576bdc1
Om Kumar Chaudhary <127199200+omkumarchaudhary@users.noreply.github.com>
2025-05-16 18:06:26 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,18 +5,17 @@ do
   echo "Starting stream..."
 
   ffmpeg \
-    -re -loop 1 -framerate 30 -i background.jpg \         # 30 FPS for smooth video
-    -stream_loop -1 -i music.mp3 \                        # loop audio infinitely
-    -c:v libx264 -preset veryfast -tune stillimage \     # encode video fast & optimized for still images
-    -pix_fmt yuv420p \                                    # pixel format compatible with YouTube
-    -c:a aac -b:a 128k -ar 44100 \                        # AAC audio with standard sample rate
+    -re -loop 1 -framerate 30 -i background.jpg \
+    -stream_loop -1 -i music.mp3 \
+    -s 1280x720 \
+    -c:v libx264 -preset veryfast -tune stillimage \
+    -pix_fmt yuv420p \
+    -c:a aac -b:a 128k -ar 44100 \
     -f flv \
-    -maxrate 3000k -bufsize 6000k \                       # control bitrate to avoid spikes
-    -g 60 \                                               # GOP size, 2 sec keyframes at 30fps (good for streaming)
+    -maxrate 3000k -bufsize 6000k \
+    -g 60 \
     "rtmp://x.rtmp.youtube.com/live2/6y98-mazq-py6b-kayt-dzz2"
 
   echo "Restarting in 10 seconds..."
   sleep 10
 done
-
-
